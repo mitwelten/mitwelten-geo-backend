87ebdf6961b6a7e069495e0160916d108e590b96
Martin Lacayo <mlacayoemery@gmail.com>
2021-04-22 09:11:37 +0200

    Added prefix path to GDAL installation configuration

--- setup/install-gdal.sh
+++ setup/install-gdal.sh
@@ -1,6 +1,6 @@
 sudo apt-get install build-essential autoconf automake libtool curl make g++ unzip
 sudo apt install libspatialindex-dev
-sudo apt-get install proj-bin python-dev
+sudo apt-get install proj-bin python2-dev python3-dev
 sudo pip3 install numpy
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 sudo ldconfig
@@ -23,7 +23,7 @@ cd ..
 wget http://download.osgeo.org/gdal/3.2.2/gdal-3.2.2.tar.gz
 tar -xvf gdal-3.2.2.tar.gz
 cd gdal-3.2.2
-./configure --with-python
+./configure --prefix=g/usr/local/gdal/gdal-3.2.2 --with-python
 make
 sudo make install
-sudo pip3 install /data/share/gdal-3.2.2/swig/python
+sudo pip3 install gdal-3.2.2/swig/python
