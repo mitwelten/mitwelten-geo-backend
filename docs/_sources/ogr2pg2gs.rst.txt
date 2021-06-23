d16bfe23698f6e898a0f9f9fbbe7cb7e8d606308
Martin Lacayo <mlacayoemery@gmail.com>
2021-06-24 01:43:08 +0200

    Setup Sphinx template

---
+++ docs/_sources/ogr2pg2gs.rst.txt
@@ -0,0 +1,5 @@
+OGR2PG2GS
+=========
+
+.. automodule:: ogr2pg2gs
+    :members:
---
+++ docsrc/conf.py
@@ -0,0 +1,27 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath('..'))
+
+project = 'Mitwelten'
+
+extensions = [
+#    'sphinx.ext.extlinks',
+    'sphinx.ext.autodoc',
+#    'sphinx.ext.todo',
+#    'sphinx.ext.mathjax',
+#    'sphinx.ext.viewcode',
+#    'sphinx.ext.napoleon',
+#    'pywps.ext_autodoc'
+]
+
+exclude_patterns = ['_build']
+source_suffix = '.rst'
+master_doc = 'index'
+
+pygments_style = 'sphinx'
+
+#html_static_path = ['_static']
+
+htmlhelp_basename = 'MWdoc'
+ 
